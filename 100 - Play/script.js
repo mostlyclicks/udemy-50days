0e2797177025dffd6faa1952bc0255eb24719869
Carlos Interone <carlos@mostlyclicks.com>
2021-04-29 06:56:33 -0600

    initial commit

---
+++ 100 - Play/script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('button')
+const nav = document.querySelector('nav')
+
+btn.addEventListener('click', () => {
+    nav.style.backgroundColor = "#ededed"
+})
+
+
+
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// const container = document.querySelector('.container')
+
+// open.addEventListener('click', () => {
+//     container.classList.add('show-nav')
+// })
+
+// close.addEventListener('click', () => {
+//     container.classList.remove('show-nav')
+// })
